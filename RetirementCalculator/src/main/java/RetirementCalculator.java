42f917aac1515a415dbee444877f65da7b82e32d
Adrian Curtis <adrian.curtis@kingfisher.com>
2016-08-02 16:12:37 +0100

    Updates to all the java files i've been working on

--- RetirementCalculator/src/main/java/RetirementCalculator.java
+++ RetirementCalculator/src/main/java/RetirementCalculator.java
@@ -1,27 +1,64 @@
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class RetirementCalculator {
 
     public static void main(String[] args) {
 
-        Scanner read = new Scanner(System.in);
         String ageInput;
         String retirementageInput;
+        int yearsToRetirement;
+        int yearOfRetiremet;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
 
+        Scanner read = new Scanner(System.in);
 
         System.out.println("What is your age? (if you don't mind me asking of course :)");
+        while(!read.hasNextInt()) {
+            System.out.println("Please enter a whole number (ie 2 not 2.0 or two)");
+        }
         ageInput = read.next();
         int age = Integer.parseInt(ageInput);
 
 
 
         System.out.println("At what age would you like to retire? (if you will ever be allowed to!)");
+        while(!read.hasNextInt()) {
+            System.out.println("Please enter a whole number (ie 2 not 2.0 or two)");
+        }
         retirementageInput = read.next();
         int retirementage = Integer.parseInt(retirementageInput);
 
-        
+        yearsToRetirement = yearsToRetirement(age, retirementage);
+
+        yearOfRetiremet = yearOfRetiremet(age, retirementage);
+
+        if(yearsToRetirement <= 0){
+            System.out.println("You could have already retired!");
+        }
+
+        System.out.println("You have " + yearsToRetirement + " years left until you can retire.");
+        System.out.println("It's " + year + ", so you can retire in" +yearOfRetiremet);
 
 
+    }
+
+    public static int yearsToRetirement(int age, int retirementage){
+
+        int yearsToRetirement;
+
+        yearsToRetirement = (retirementage - age);
+
+        return yearsToRetirement;
+    }
+
+    public static int yearOfRetiremet(int age, int retirementage){
+
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        int yearOfRetirement = (retirementage - age) + year;
+
+        return yearOfRetirement;
 
 
     }
--- RetirementCalculator/src/test/java/RetirementCalculatorTest.java
+++ RetirementCalculator/src/test/java/RetirementCalculatorTest.java
@@ -1,9 +1,25 @@
-import static org.junit.Assert.*;
-import org.junit.Ignore; 
-import org.junit.Test;  
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 public class RetirementCalculatorTest {
     @Test
     public void number1() {
-        assertEquals(I, J);
+        assertEquals(25, RetirementCalculator.yearsToRetirement(40,65));
+    }
+
+    @Test
+    public void YearOfRetirement() {
+        assertEquals(2041, RetirementCalculator.yearOfRetiremet(40,65));
+    }
+
+
+    @Test
+    public void YearOfRetirementforme() {
+        assertEquals(2047, RetirementCalculator.yearOfRetiremet(34, 65));
+    }
+
+        @Test
+        public void YearOfRetirementforme1() {
+            assertEquals(31, RetirementCalculator.yearsToRetirement(34,65));
     }
 }
